6bfd6b5141d4065d6570a7f65c8576c165aaca7e
alexeykosh <alexeykochevoy@gmail.com>
2021-10-11 20:32:55 +0200

    corrected some errors

--- preprocessing1.py
+++ preprocessing1.py
@@ -21,38 +21,36 @@ def char_to_unicode(char):
 
 
 def convert_list_of_words(file):
-	'''
-	Transforming word frequencies to letter frequencies
-	'''
-	name = re.match(r'^(\w+)', file.split('/')[1]).group(0) # get lang name
-	freq = pd.read_csv(file) # frequencies
-	freq['textfile'] = freq['textfile'].astype('str') # transform words to str type
-	freq['textfile'] = freq['textfile'].apply(list) # separate words into letters
+	name = re.match(r'^(\w+)', file.split('/')[1]).group(0)
+	freq = pd.read_csv(file)
+	freq['textfile'] = freq['textfile'].astype('str')
+	freq['textfile'] = freq['textfile'].apply(list)
 	freq = freq.explode('textfile')[['textfile', 'Freq']].\
-		groupby('textfile').\
-		sum().sort_values(by='Freq', ascending=False).reset_index() # word frequencies to letter freq
-	freq['unicode'] = freq['textfile'].apply(char_to_unicode) # get unicode pointers
-	freq['Freq_rel'] = freq['Freq']/sum(freq['Freq']) # get relative frequencies
-	freq['lang'] = str(name) # add language info
-	freq = pd.concat([freq.set_index('unicode'), complexity],
-		axis=1, sort=True).dropna(subset=['Freq']).reset_index() # concat with complexity dataset
-	freq['rank'] = freq['Freq'].rank() # get frequency rank
+	groupby('textfile').\
+	sum().sort_values(by='Freq', ascending=False).reset_index()
+	freq['unicode'] = freq['textfile'].apply(char_to_unicode)
+	freq['lang'] = str(name)
+	freq['file'] = file
 	return freq
 
 
-for filename in os.listdir('FreqDists_50K/'):
-    dfs.append(convert_list_of_words('FreqDists_50K/'+filename))
+exceptions = ['eng-x-bible-scriptures.csv']
 
+for filename in os.listdir('FreqDists_50K/'):
+	if filename not in exceptions:
+		dfs.append(convert_list_of_words('FreqDists_50K/'+filename))
 
 res_full = pd.concat(dfs)
-res_full_cl = res_full.dropna(subset=['PCComplexity'])
-res_full_cl = res_full_cl[~res_full_cl['folder'].isin(['Cyrl', 'Latn'])]
-ref = pd.DataFrame(res_full_cl.groupby(["lang", "folder"]).size().reset_index())
-langs = ref[~ref.duplicated(subset='lang', keep=False)]['lang']
-langs_d = ref[ref.duplicated(subset='lang', keep=False)]['lang']
-res_full_cl_no_dub = res_full_cl[res_full_cl['lang'].isin(langs)]
-res_full_cl_no_dub['folder_lang'] = res_full_cl_no_dub['folder']\
-	+ '_' + res_full_cl_no_dub['lang']
-res_full_cl_no_dub.to_csv('bentz_final.csv')
+res_full['Sum_count'] = res_full['Freq'].\
+groupby(res_full['lang']).transform('sum')
+res_full['Rel_freq'] = res_full['Freq']/res_full['Sum_count']
+res_full = pd.DataFrame(pd.DataFrame(res_full.\
+	groupby(['lang', 'unicode', 'textfile'])[['Freq']].\
+	sum()).reset_index()).set_index(['unicode'])
+res_full = res_full.merge(complexity,
+	left_index=True, right_index=True).reset_index()
+res_full_cl = res_full[~res_full['folder'].isin(['Cyrl', 'Latn'])]
+res_full_cl = res_full_cl.dropna(subset=['PCComplexity'])
+res_full_cl.to_csv('bentz_final.csv')
 
-print(res_full_cl_no_dub.shape)
+print(res_full_cl.shape)
